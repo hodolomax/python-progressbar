5b549dbdf6c3c502fe6d7d705cba5e2a74c66b9f
hodolomax <hodolomax@gmail.com>
2020-12-24 13:26:20 +0300

    Update progress-bar.py

--- progress-bar.py
+++ progress-bar.py
@@ -1,15 +1,15 @@
 import tqdm
 
-max = 400000000 # Buraya ne kadar çok sıfır koyarsan o kadar uzun sürer bar
+max = 100000000 # Buraya ne kadar çok sıfır koyarsan o kadar uzun sürer bar
 start = 0
 batch_size = 10
 # moduleId=['220']
 docs =-1
 calculate = []
-
-for start in tqdm.tqdm(range(0, max + batch_size, batch_size)):
-    batch_size +=  10
-    docs += 1
-
-
-print("{0} entities have been changed".format(docs))
+for i in range(30):
+    for start in tqdm.tqdm(range(0, max + batch_size, batch_size)):
+        batch_size +=  10
+        docs += 1
+    print("{0} entities have been changed".format(docs))
+    batch_size = 10
+    start= 0
